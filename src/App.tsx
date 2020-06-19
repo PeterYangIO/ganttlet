e308e5aa2bf2b3c8445de86e76a69bb94c2e00b3
kingdumbb <35203166+kingdumbb@users.noreply.github.com>
2020-06-19 02:02:19 -0500

    Add mockups for dashboard, signin, signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
+import SignIn from './components/signin/SignIn';
+import SignUp from './components/signup/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/signup">
+                        <SignUp />
+                    </Route>
+                    <Route path="/signin">
+                        <SignIn />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
