fd1403c84ed0467f39b6f9c2404b239d447c5c7e
JosephPallipadan <pallipadanjoseph@gmail.com>
2020-06-20 12:08:29 +0400

    Setup firebase config object, Firebase class with helper functions

---
+++ src/components/Firebase/firebase.ts
@@ -0,0 +1,48 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+    apiKey: 'AIzaSyAFCnshUKjdme_1QFXaSFzkTxjLtGpztF0',
+    authDomain: 'ganttlett.firebaseapp.com',
+    databaseURL: 'https://ganttlett.firebaseio.com',
+    projectId: 'ganttlett',
+    storageBucket: 'ganttlett.appspot.com',
+    messagingSenderId: '783372525556',
+    appId: '1:783372525556:web:37eb04f4ccfdbd62fae3a1',
+    measurementId: 'G-WH23WGKNYZ',
+};
+
+class Firebase {
+    auth: app.auth.Auth;
+    db: app.database.Database;
+    constructor() {
+        app.initializeApp(config);
+        this.auth = app.auth();
+        this.db = app.database();
+    }
+
+    /* Auth API */
+
+    doCreateUserWithEmailAndPassword = (email: string, password: string): Promise<app.auth.UserCredential> =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email: string, password: string): Promise<app.auth.UserCredential> =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = (): Promise<void> => this.auth.signOut();
+
+    doPasswordReset = (email: string): Promise<void> => this.auth.sendPasswordResetEmail(email);
+
+    // https://stackoverflow.com/questions/58496176/how-to-properly-annotate-object-to-fix-object-is-possibly-null
+    doPasswordUpdate = (password: string): Promise<void> =>
+        this.auth.currentUser ? this.auth.currentUser.updatePassword(password) : Promise.resolve();
+
+    /* -------- */
+
+    /* Auth API */
+
+    /* -------- */
+}
+
+export default Firebase;
